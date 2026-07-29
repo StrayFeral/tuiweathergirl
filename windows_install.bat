ab0350d2afd8f37f4b3c51d32032671eba8c7c36
Evgueni Antonov <strayferalmusic@gmail.com>
2026-07-29 14:51:19 +0300

    Fixed Windows installation script

--- windows_install.bat
+++ windows_install.bat
@@ -2,21 +2,31 @@
 setlocal
 
 echo .
-echo "Installing TUIWEATHERGIRL..."
+echo Installing TUIWEATHERGIRL...
 echo .
 
+echo Testing for presence of Python...
 where python >nul 2>nul
 if %ERRORLEVEL% neq 0 (
-    echo Error: Python is not installed or not added to your PATH.
+    echo ERROR: Python is not installed or not added to your PATH.
     echo Please install Python and try again.
+    echo .
+    echo You can install it from here:
+    echo https://www.python.org/downloads/windows/
+    echo .
+    pause
     exit /b 1
 )
 
+echo Installing python components...
 pip install windows-curses Babel tzdata requests
 
-echo @python "%%LOCALAPPDATA%%\Microsoft\WindowsApps\tuiweathergirl.py" %%* > "%LOCALAPPDATA%\Microsoft\WindowsApps\tuiweathergirl.bat"
+echo Installing app...
 copy tuiweathergirl.py "%LOCALAPPDATA%\Microsoft\WindowsApps\tuiweathergirl.py"
 
 echo .
-echo "DONE."
+echo DONE.
+echo .
+
+pause
 
