3c7d7e2bdb6dc7e0994bc6463f0561a2af073156
Evgueni Antonov <strayferalmusic@gmail.com>
2026-08-22 03:44:13 +0300

    Fixed exception logging message

--- tuiweathergirl.py
+++ tuiweathergirl.py
@@ -40,7 +40,7 @@ from packaging.version import parse as parse_version
 # I intentionally left these here, as I tend to change them time to time
 # and don't want to scroll too much to find them
 
-__version__: str = "1.2.2"
+__version__: str = "1.2.3"
 
 DEBUG_MODE: bool = False
 DEFAULT_VIEW: str = "dashboard"
@@ -3453,7 +3453,7 @@ class UpdateManager:
             self._restart_application()
 
         except Exception as e:
-            print(f"Failed to update to version {newer_version}: {e}")
+            self.logger.exception(f"Failed to update to version {newer_version}: {e}")
 
     def force_update(self) -> None:
         """Force update the application"""
