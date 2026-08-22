51e3c48dbe47a75683730f17f2366f1067f3b2ac
Evgueni Antonov <strayferalmusic@gmail.com>
2026-08-22 04:38:04 +0300

    Fake version to test app update

--- tuiweathergirl.py
+++ tuiweathergirl.py
@@ -43,7 +43,7 @@ from packaging.version import parse as parse_version
 # I intentionally left these here, as I tend to change them time to time
 # and don't want to scroll too much to find them
 
-__version__: str = "1.2.10"
+__version__: str = "1.2.11"
 
 DEBUG_MODE: bool = False
 DEFAULT_VIEW: str = "dashboard"
