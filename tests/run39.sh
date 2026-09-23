7302f0d0c428550fbb5470f7aac69970aac461c8
Evgueni Antonov <strayferalmusic@gmail.com>
2026-09-23 20:12:27 +0300

    Added: Python 3.9 compatibility test; Screen reading software test (for visually impaired users).

---
+++ tests/run39.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Python 3.9 compatibility test
+sudo docker run --rm -it python:3.9-bookworm bash
---
+++ tests/voice_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# ./tuiweathergirl.py --view simple | spd-say -e -t female1 -r 10
+./tuiweathergirl.py --view simple | spd-say -r 20
